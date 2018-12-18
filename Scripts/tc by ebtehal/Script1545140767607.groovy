5e933f46ebcc7ad5af6e33da3c665919a55d9ef9
AzzaSalahEldin <45813941+AzzaSalahEldin@users.noreply.github.com>
2018-12-18 16:03:01 +0200

    er

--- Scripts/tc by ebtehal/Script1545140767607.groovy
+++ Scripts/tc by ebtehal/Script1545140767607.groovy
@@ -13,9 +13,5 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-<<<<<<< HEAD
-WebUI.acceptAlert()
-=======
 WebUI.sendKeys(findTestObject(null), '')
->>>>>>> branch 'master' of https://github.com/AzzaSalahEldin/19112018
 
